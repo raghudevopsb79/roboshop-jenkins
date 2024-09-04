fa88970590edb8d876c6de99753e8bbf14a4db71
Raghu K <raghuk.vit@gmail.com>
2024-09-04 07:59:29 +0530

    RoboShop Terraform & Kubernetes Code

--- vars/cipipeline.groovy
+++ vars/cipipeline.groovy
@@ -1,47 +1,60 @@
 def call() {
-  pipeline {
-    agent any
 
-    stages {
-
-      stage('Build') {
-        steps {
-          sh 'echo Hello'
-        }
-      }
-
-      stage('Unit Tests') {
-        steps {
-          sh 'echo Hello'
-        }
-      }
-
-      stage('Integration Tests') {
-        steps {
-          sh 'echo Hello'
-        }
-      }
-
-      stage('Code Quality') {
-        steps {
-          sh 'echo Hello'
-        }
-      }
-
-      stage('Security Checks') {
-        steps {
-          sh 'echo Hello'
-        }
-      }
-      stage('Deploy to Dev') {
-        steps {
-          sh 'echo Hello'
-        }
-      }
-
-
+  node() {
+    stage(Build) {
+      sh 'ls -l'
     }
-
   }
+
+
+
+
+
+
+
+//  pipeline {
+//    agent any
+//
+//    stages {
+//
+//      stage('Build') {
+//        steps {
+//          sh 'echo Hello'
+//        }
+//      }
+//
+//      stage('Unit Tests') {
+//        steps {
+//          sh 'echo Hello'
+//        }
+//      }
+//
+//      stage('Integration Tests') {
+//        steps {
+//          sh 'echo Hello'
+//        }
+//      }
+//
+//      stage('Code Quality') {
+//        steps {
+//          sh 'echo Hello'
+//        }
+//      }
+//
+//      stage('Security Checks') {
+//        steps {
+//          sh 'echo Hello'
+//        }
+//      }
+//      stage('Deploy to Dev') {
+//        steps {
+//          sh 'echo Hello'
+//        }
+//      }
+//
+//
+//    }
+//
+//  }
 //
 }
