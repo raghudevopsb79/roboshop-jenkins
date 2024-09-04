d8e015c1d172efc1399f221d4399e425fe3f3780
Raghu K <raghuk.vit@gmail.com>
2024-09-04 08:07:38 +0530

    RoboShop Terraform & Kubernetes Code

--- vars/cipipeline.groovy
+++ vars/cipipeline.groovy
@@ -1,6 +1,19 @@
 def call() {
 
   node() {
+
+    stage('Code Checkout') {
+      sh 'find . | grep "^./" |xargs rm -rf'
+
+      if(env.TAG_NAME ==~ ".*") {
+        env.gitbrname = "refs/tags/${env.TAG_NAME}"
+      } else {
+        env.gitbrname = "${env.BRANCH_NAME}"
+      }
+      checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: "https://github.com/raghudevopsb79/${env.appName}"]], branches: [[name: gitbrname]]], poll: false
+
+    }
+
     stage('Build') {
       sh 'ls -l'
     }
